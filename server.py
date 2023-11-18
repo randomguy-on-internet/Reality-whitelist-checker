cd2d633fcf2c554c3172dbd5f6f43dcf49e647a1
who knows? <79375739+randomguy-on-internet@users.noreply.github.com>
2023-11-18 14:44:33 +0330

    Update server.py

--- server.py
+++ server.py
@@ -3,11 +3,19 @@ import subprocess
 import json
 
 def change_sni(sni):
-    with open("sing-box_config.json", "r") as f:
+
+    # reading from xray configuration file
+    with open("/usr/local/etc/xray/config.json", "r") as f:
         data = json.load(f)
-    data['inbounds'][0]['tls']['server_name'] = sni
-    data['inbounds'][0]['tls']['reality']['handshake']['server'] = sni
-    with open("sing-box_config.json", "w") as f:
+        
+    # SNI - its a list of sni's like ["a.com","b.com"]    
+    data['inbounds'][0]['streamSettings']['realitySettings']['serverNames'] = sni.split(",")
+
+    # dest, change it if you know what you are doing!
+    data['inbounds'][0]['streamSettings']['realitySettings']['dest'] = "1.1.1.1:443"
+    
+    # saving sni and destination to xray configuration file
+    with open("/usr/local/etc/xray/config.json", "w") as f:
         json.dump(data, f)
 
 
@@ -18,7 +26,7 @@ app = Flask(__name__)
 def handle_post():
     text = request.data.decode('utf-8')
     change_sni(text)
-    subprocess.run(['systemctl', 'restart', 'sing-box', '--now'])
+    subprocess.run(['systemctl', 'restart', 'xray', '--now'])
     return 'Changed SNI'
 
 if __name__ == '__main__':
